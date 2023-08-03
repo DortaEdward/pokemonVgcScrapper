f352e1e36faaafa7292fcea4dac60cadd8de8f4c
Edward Dorta <dortaedward@gmail.com>
2023-08-03 08:22:19 -0400

    Working on a new method to parse the table

--- python/scripts/moveset.py
+++ python/scripts/moveset.py
@@ -1,6 +1,60 @@
 from os import path, getcwd
 import json
+
 file_path = path.join(getcwd(),'python/data/moveset/gen9vgc2023regulationd-1760.txt')
+row_sep = "+"
+keys = ['Abilities','Moves','Teammates','Spreads','Items']
+
+def split(file_path):
+    with open(file_path,'r') as file:
+        cut = []
+        for x,line in enumerate(file):
+            
+            if line.startswith(' +'):
+                cut.append(x)          
+        file.close()
+    return cut
+
+def key_index(file):
+    with open(file_path,'r') as file:
+        key_value = []
+        for x,line in enumerate(file):   
+            for key in keys:
+                if key in line:
+                    value = {
+                        "index":x,
+                        "key":key
+                    }
+                    key_value.append(value)
+        file.close()    
+    return key_value
+
+
+
+def parse_data(cut_arr,key_index, file_path):
+    final_data = []
+    with open(file_path,'r') as file:
+        for i,line in enumerate(file):
+            temp_data = {}
+            active = False
+            if i in cut_arr:
+                active = True
+                continue
+            for item in key_index:
+                if item['index'] == i:
+                    key = item['key']
+                    temp_data[key] = []
+                    final_data.append(temp_data)
+                
+        print(final_data)
+        file.close()
+
+def main():
+    cut_arr = split(file_path)
+    ki = key_index(file_path)
+    parse_data(cut_arr,ki,file_path)
+
+main()
 
 '''
 def parse_table_data(lines):
@@ -48,15 +102,52 @@ def parse_table_data(lines):
     with open('output.json', 'w') as file:
         file.write(json_object)
 
+        
+        =========
+start = False
+        key_picked = None
+        obj = {}
+        for line in f:
+            if len(line) > 2 and line[1] == row_sep:
+                if not start:
+                    start = True
+                else:
+                    start = False
+            else:
+                if key_picked == None:
+                    for key in keys:
+                        if key in line:
+                            key_picked = key
+                else:
+                    while line.
+                    print('passed')
+=================================
+   lines = table_data.strip().split("\n")
+    data_arr = []
+    current_section = None
 
-
-def main():
-    with open(file_path, 'r') as file:
-        lines = file.read()
-
-    parsed_data = parse_table_data(lines)
-    # json_data = json.dumps(parsed_data, indent=2)
-
-
-main()
+    for line in lines:
+        data_dict = {}
+        temp_dict = {}
+        if line[1] == "|":
+            parts = line.strip("| ").split(":")
+            if len(parts) == 2:
+                data_key = parts[0].strip()
+                data_value = parts[1].strip()
+                if current_section is None:
+                    current_section = data_key
+                    data_dict[current_section] = {}
+                else:
+                    temp_dict[data_key] = data_value
+                    data_dict[current_section]= temp_dict
+            elif len(parts) == 1 and current_section is not None:
+                data_value = parts[0].strip()
+                if current_section not in data_dict:
+                    data_dict[current_section] = []
+                data_dict[current_section] = data_value
+            data_arr.append(data_dict)
+    
+    for data in data_arr:
+        print(data)
+    return data_arr
 '''
